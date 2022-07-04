ef97107ec436c65fe21068d007ce43069c3ce8eb
Igor de Almeida <igoor1205@gmail.com>
2022-07-04 10:26:51 -0300

    feat: add errors on BaseException

--- src/main/java/com/liven/userapi/exceptions/BaseException.java
+++ src/main/java/com/liven/userapi/exceptions/BaseException.java
@@ -1,8 +1,11 @@
 package com.liven.userapi.exceptions;
 
+import java.util.Map;
+
 public class BaseException extends Exception {
   public String message;
   public int statusCode;
+  public Map<String, String> errors;
   public String stack;
 
   public BaseException(String message, int statusCode) {
@@ -10,8 +13,13 @@ public class BaseException extends Exception {
     this.statusCode = statusCode;
   }
 
-  public BaseException(String message, int statusCode, String stack) {
+  public BaseException(String message, int statusCode, Map<String, String> errors) {
     this(message, statusCode);
+    this.errors = errors;
+  }
+
+  public BaseException(String message, int statusCode, Map<String, String> errors, String stack) {
+    this(message, statusCode, errors);
     this.stack = stack;
   }
 }
--- src/main/java/com/liven/userapi/exceptions/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.liven.userapi.exceptions;
-
-public class ValidationException extends BaseException {
-  public ValidationException(String message) {
-    super(message, 400);
-  }
-
-}

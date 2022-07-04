adec268ce828553ee79540e1497f81a6e6e89177
Igor de Almeida <igoor1205@gmail.com>
2022-07-04 10:27:45 -0300

    refactor: upgrade GlobalExceptionHandler to handle with validation errors and BaseException errors

--- src/main/java/com/liven/userapi/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/liven/userapi/controllers/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.liven.userapi.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -15,17 +20,38 @@ import com.liven.userapi.utils.ApiResponse;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @ExceptionHandler({ BaseException.class })
+  protected ResponseEntity<Object> handleBaseException(BaseException ex, WebRequest request) {
+    return ResponseEntity.status(ex.statusCode).body(ApiResponse.getErrorResponse(ex));
+  }
+
   @Override
   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
       HttpStatus status, WebRequest request) {
 
-    if (ex instanceof BaseException) {
-      BaseException exception = (BaseException) ex;
-      return ResponseEntity.status(status).body(ApiResponse.getErrorResponse(exception));
+    if (ex instanceof MethodArgumentNotValidException) {
+      MethodArgumentNotValidException methodArgumentNotValidException = (MethodArgumentNotValidException) ex;
+
+      Map<String, String> errors = new HashMap<>();
+
+      methodArgumentNotValidException.getBindingResult().getAllErrors().forEach((error) -> {
+        String fieldName = ((FieldError) error).getField();
+        String errorMessage = error.getDefaultMessage();
+        errors.put(fieldName, errorMessage);
+      });
+
+      BaseException exception = new BaseException(
+          "Validation error",
+          HttpStatus.BAD_REQUEST.value(),
+          errors);
+
+      return ResponseEntity
+          .status(HttpStatus.valueOf(exception.statusCode))
+          .body(ApiResponse.getErrorResponse(exception));
     } else {
       BaseException exception = new BaseException(
           ex.getMessage(),
-          HttpStatus.INTERNAL_SERVER_ERROR.value(), List.of(ex.getStackTrace()).toString());
+          HttpStatus.INTERNAL_SERVER_ERROR.value(), null, List.of(ex.getStackTrace()).toString());
 
       return ResponseEntity
           .status(HttpStatus.valueOf(exception.statusCode))
